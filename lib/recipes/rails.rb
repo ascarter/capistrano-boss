83743fdbca541d5d986392d7ece34eb1e517c162
Andrew Carter <andrew.carter@gettyimages.com>
2009-12-01 15:38:26 -0800

    Shorten up the db variable names. Added a top level rails::config task that could capture other Rails setup tasks

--- lib/recipes/rails.rb
+++ lib/recipes/rails.rb
@@ -1,32 +1,37 @@
 Capistrano::Configuration.instance.load do
   namespace :rails do
+    after "deploy:setup", "rails:config"
     after "deploy:update_code", "rails:deploy_config"
-    
+
+    task :config, :roles => :app do
+      rails::config_database
+    end
+
     desc "Create a database.yml file in shared configuration"
     task :config_database, :roles => :app do
       require 'yaml'
       
       # Set required fields
-      set(:rails_db_adapter, "mysql") unless exists?(:rails_db_adapter)
-      set(:rails_db_username, user) unless exists?(:rails_db_username)
-      set(:rails_db_database, "#{user}_#{rails_env}") unless exists?(:rails_db_database)
-      unless exists?(:rails_db_password)
-        set(:rails_db_password) { Capistrano::CLI.password_prompt("#{rails_env} #{rails_db_adapter} password: ") }
+      set(:db_adapter, "mysql") unless exists?(:db_adapter)
+      set(:db_username, user) unless exists?(:db_username)
+      set(:db_database, "#{user}_#{rails_env}") unless exists?(:db_database)
+      unless exists?(:db_password)
+        set(:db_password) { Capistrano::CLI.password_prompt("#{rails_env} #{db_adapter} password: ") }
       end
 
       # Create database config
       db_config = {
         "#{rails_env}" => {
-          "adapter" => rails_db_adapter,
-          "database" => rails_db_database,
-          "username" => rails_db_username,
-          "password" => rails_db_password
+          "adapter" => db_adapter,
+          "database" => db_database,
+          "username" => db_username,
+          "password" => db_password
         }
       }
 
       # Add any optional overrides
       %w[host port socket encoding sslkey sslcert sslcapath sslcipher pool].each do |attribute|
-        varname = "rails_db_#{attribute}".to_sym
+        varname = "db_#{attribute}".to_sym
         if exists?(varname)
           db_config["#{rails_env}"][attribute] = fetch(varname)
         end
