ba0435ad77575c16150bf5c19cb287424eb8d9f9
Andrew Carter <andrew.carter@gettyimages.com>
2009-12-01 07:24:58 -0800

    Add support to subversion scm to specify a tag or branch

---
+++ lib/capistrano/recipes/deploy/scm/subversion.rb
@@ -0,0 +1,23 @@
+require 'capistrano/recipes/deploy/scm/base'
+
+module Capistrano
+  module Deploy
+    module SCM
+      class Subversion < Base
+        # Returns working subversion repository path
+        # Defaults to trunk
+        # If variable :tag or :branch are set, path is adjusted
+        # Repository path should refer to root of standard svn layout
+        def repository
+          if variable(:tag)
+            "#{variable(:repository)}/tags/#{variable(:tag)}"
+          elsif variable(:branch)
+            "#{variable(:repository)}/branches/#{variable(:branch)}"
+          else
+            "#{variable(:repository)}/trunk"
+          end
+        end
+      end
+    end
+  end
+end
