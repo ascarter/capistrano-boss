9a49f23e4fa030c1ea024751603c1106a48cd2ee
Andrew Carter <andrew.carter@gettyimages.com>
2009-12-01 07:40:55 -0800

    Wrap passenger recipe with instance load

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -1,7 +1,9 @@
-namespace :passenger do
-  desc "Restart Rails application"
-  task :restart, :roles => :app do
-    run "touch #{current_release}/tmp/restart.txt"
+Capistrano::Configuration.instance.load do
+  namespace :passenger do
+    desc "Restart Rails application"
+    task :restart, :roles => :app do
+      run "touch #{current_release}/tmp/restart.txt"
+    end
   end
 end
 
