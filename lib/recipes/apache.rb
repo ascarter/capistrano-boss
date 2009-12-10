4b1bfbb41f744edeac5d77f40cccd3979a1b7d71
Andrew Carter <andrew.carter@gettyimages.com>
2009-12-09 17:32:21 -0800

    Define full suite of apachectl commands

--- lib/recipes/apache.rb
+++ lib/recipes/apache.rb
@@ -2,24 +2,35 @@ Capistrano::Configuration.instance.load do
   namespace :apache do
     set :apachectl, 'apachectl'
 
-    desc "Restart Apache web service"
-    task :restart, :roles => :app do
-      sudo "#{apachectl} restart", :pty => true
-    end
-
     desc "Start Apache web service"
     task :start, :roles => :app do
-      sudo "#{apachectl} start", :pty => true
+      apachectl_cmd("start")
     end
   
     desc "Stop Apache web service"
     task :stop, :roles => :app do
-      sudo "#{apachectl} stop", :pty => true
+      apachectl_cmd("stop")
     end
 
-    desc "Check status of Apache web service"
-    task :status, :roles => :app do
-      run "#{apachectl} status"
+    desc "Restart Apache web service"
+    task :restart, :roles => :app do
+      apachectl_cmd("restart")
     end
+    
+    desc "Graceful Apache web service"
+    task :graceful, :roles => :app do
+      apachectl_cmd("graceful")
+    end
+
+    desc "Graceful-stop Apache web service"
+    task :graceful-stop, :roles => :app do
+      apachectl_cmd("graceful-stop")
+    end    
+  end
+  
+  # Helpers
+  
+  def apachectl_cmd(action)
+    sudo "#{apachectl} #{action}", :pty => true
   end
 end
