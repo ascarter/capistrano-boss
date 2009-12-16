9d82b60fc22cfbf20c87261baceac5ebcac4cf70
Andrew Carter <ascarter@gmail.com>
2009-12-16 11:25:33 -0800

    Fixed timestamp reference

--- lib/recipes/deploy.rb
+++ lib/recipes/deploy.rb
@@ -6,7 +6,7 @@ namespace :deploy do
       abort "Missing authorized_keys file at #{keyfile}"
     end
     run "mkdir -p -m 700 .ssh"
-    run "if [ -e .ssh/authorized_keys ]; then cp .ssh/authorized_keys .ssh/authorized_keys.#{CapistranoBoss::Log.timestamp}; fi;"
+    run "if [ -e .ssh/authorized_keys ]; then cp .ssh/authorized_keys .ssh/authorized_keys.#{CapistranoBoss.timestamp}; fi;"
     put File.new(keyfile).read, ".ssh/authorized_keys", :mode => 0600
   end
 end
