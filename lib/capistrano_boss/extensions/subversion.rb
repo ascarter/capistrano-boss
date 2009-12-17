6db179b354c031868356473d111e81a45812cada
Andrew Carter <ascarter@gmail.com>
2009-12-16 20:31:44 -0800

    Added :svn_explicit_path setting to overrided trunk/branch/tag handling in Subversion

--- lib/capistrano_boss/extensions/subversion.rb
+++ lib/capistrano_boss/extensions/subversion.rb
@@ -9,7 +9,9 @@ module Capistrano
         # If variable :tag or :branch are set, path is adjusted
         # Repository path should refer to root of standard svn layout
         def repository
-          if variable(:tag)
+          if exists?(:svn_explicit_path)
+            variable(:repository)
+          elsif variable(:tag)
             "#{variable(:repository)}/tags/#{variable(:tag)}"
           elsif variable(:branch)
             "#{variable(:repository)}/branches/#{variable(:branch)}"
